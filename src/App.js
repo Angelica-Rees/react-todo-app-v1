eef8fbe34a72bf78b1063e142b14b29d31a64e82
Angelica Rees <angelkdrees@gmail.com>
2023-02-25 16:38:49 -0500

    Functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ToDoList from './components/ToDoList';
 
 function App() {
   return (
-    <div className="App">
-      <h1>To Do App</h1>
+    <div className="ToDo-App">
+      <ToDoList />
     </div>
   );
 }
